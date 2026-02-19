1e0f96ad6eb27715384db40c4fbe3f302cbbcc4f
Tanner Juby <tanner@mobilizeyourtech.com>
2026-02-19 16:16:28 -0700

    Removes the github share button

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -28,7 +28,6 @@ import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
@@ -332,8 +331,6 @@ export function NotionPage({
         pageAside={pageAside}
         footer={footer}
       />
-
-      <GitHubShareButton />
     </>
   )
 }
